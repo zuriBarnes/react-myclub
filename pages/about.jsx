4b2b0c501f069e7823dc0ed94c9c4085f839e778
zuriBarnes <zurikiser@gmail.com>
2023-03-28 15:52:37 -0400

    added test products

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,13 +1,18 @@
-import LoginForm from "@/components/LoginForm";
-
 export default function About() {
-  let isLoggedIn = false;
-  let content;
-  if (isLoggedIn) {
-    content = <h2>Welcome to the App</h2>;
-  } else {
-    content = <LoginForm />;
-  }
+  const products = [
+    { title: "Cabbage", id: 1 },
+    { title: "Garlic", id: 2 },
+    { title: "Apple", id: 3 },
+  ];
 
-  return <>{content}</>;
+  const listItems = products.map((product) => (
+    <li key={product.id}>{product.title}</li>
+  ));
+
+  return (
+    <section className="about">
+      <h2>About</h2>
+      <ul>{listItems}</ul>
+    </section>
+  );
 }
